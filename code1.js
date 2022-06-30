888ef1bb8f62e2932babfa500dc4d8a23693b969
michael volkov <michaeljvolkov@gmail.com>
2022-06-30 11:25:59 -0400

    to push file to github

---
+++ code1.js
@@ -0,0 +1,10 @@
+let accountBalance = 1000
+//I spent 1$ at the park when I went for a walk
+accountBalance--
+console.log(`I spend a dollar to buy water and now my new balance is ${accountBalance}`)
+
+let myAge = 23
+let myName = 'Michael'
+myName++
+//Today is my birthday
+console.log(`Since today is the birthday of ${myName}, he is now ${myAge}`)
